c38391e0172710e2657e8d7931279d8e419455b8
ijpark803 <ijpark@usc.edu>
2023-10-24 19:40:39 -0700

    google maps api done

--- app/src/main/java/com/example/takeaseat/MapView.java
+++ app/src/main/java/com/example/takeaseat/MapView.java
@@ -2,26 +2,51 @@ package com.example.takeaseat;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MapView#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
-public class MapView extends Fragment {
+public class MapView extends Fragment implements OnMapReadyCallback{
 
     View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_map_view, container, false);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.maps);
+//        mapFragment.getMapAsync(this);
+//
+//        //Inflate the layout for this fragment
+//        view = inflater.inflate(R.layout.fragment_map_view, container, false);
+//        return view;
+
+        View view=inflater.inflate(R.layout.fragment_map_view, container, false);
+
+        // Initialize map fragment
+        SupportMapFragment supportMapFragment=(SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.maps);
+
         return view;
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+
 }
