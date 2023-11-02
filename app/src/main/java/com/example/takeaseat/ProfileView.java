f2ae9d4e02de496afbc4af042141cad38de0ee1f
ijpark803 <ijpark@usc.edu>
2023-11-01 23:16:34 -0700

    logout button implemented

--- app/src/main/java/com/example/takeaseat/ProfileView.java
+++ app/src/main/java/com/example/takeaseat/ProfileView.java
@@ -3,26 +3,37 @@ package com.example.takeaseat;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileView#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ProfileView extends Fragment {
 
     View view;
     public MainActivity ma;
     TextView name, affiliation, id;
 
+    private FirebaseAuth mAuth;
+    private Button logoutbtn;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_profile_view, container, false);
         ma = (MainActivity) getActivity();
@@ -33,6 +44,27 @@ public class ProfileView extends Fragment {
         name.setText(ma.currentUser.name);
         affiliation.setText(ma.currentUser.uscAffiliation);
         id.setText(ma.currentUser.uscID);
+
+        logoutbtn = view.findViewById(R.id.logoutbtn);
+        logoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                ma.loggedIn = false;
+                replaceFragment(new Login());
+                ma.finish();
+            }
+        });
+
+
         return view;
+    }
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
