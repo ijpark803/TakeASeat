d931ef00450f11d0258ebc0ad679669e0f55fddd
Kaitlyn Werden <werden@usc.edu>
2023-11-22 21:08:04 -0600

    BlackBoxTest13 to verify profile picture functionality

---
+++ app/src/androidTest/java/com/example/takeaseat/BlackBoxTest13.java
@@ -0,0 +1,46 @@
+package com.example.takeaseat;
+
+import org.junit.Test;
+import org.junit.Rule;
+
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+
+
+
+public class BlackBoxTest13 {
+    // Written by Kaitlyn Werden
+    // Verifying profile picture functionality
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void verifySigninFields() {
+        // Start the activity
+        ActivityScenario<MainActivity> scenario = mActivityScenarioRule.getScenario();
+
+        // Navigate to profile
+        onView(withId(R.id.fragment2btn)).perform(click());
+
+        // Navigate to registration
+        onView(withId(R.id.registerbtn)).perform(click());
+
+        onView(withId(R.id.btnUpload))
+                .check(matches(isDisplayed()))
+                .check(matches(isClickable()));
+
+        // Click on the upload button
+        onView(withId(R.id.btnUpload)).perform(click());
+
+
+    }
+
+}
